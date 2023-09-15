cad178276e94583bba767da3cf7d458be46cb8a9
Saurabhmishra95 <132846454+Saurabhmishra95@users.noreply.github.com>
2023-09-15 14:08:35 +0530

    Update ApplicationDetails.java

--- src/main/java/com/experianhealth/ciam/forgerock/service/ApplicationDetails.java
+++ src/main/java/com/experianhealth/ciam/forgerock/service/ApplicationDetails.java
@@ -1,26 +1,58 @@
 package com.experianhealth.ciam.forgerock.service;
 
+import java.util.List;
+
 public class ApplicationDetails {
-    private String appId;
-    public String getAppId() {
-		return appId;
-	}
-	public void setAppId(String appId) {
-		this.appId = appId;
-	}
-	public String getAppName() {
-		return appName;
-	}
-	public void setAppName(String appName) {
-		this.appName = appName;
-	}
-	public String getAppDescription() {
-		return appDescription;
-	}
-	public void setAppDescription(String appDescription) {
-		this.appDescription = appDescription;
-	}
-	private String appName;
-    private String appDescription;
+	    private String _id;
+	    private String _rev;
+	    private String description;
+	    private List<String> mappingNames;
+	    private String url;
+	    private String icon;
+	    private String name;
+		public String get_id() {
+			return _id;
+		}
+		public void set_id(String _id) {
+			this._id = _id;
+		}
+		public String get_rev() {
+			return _rev;
+		}
+		public void set_rev(String _rev) {
+			this._rev = _rev;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		public List<String> getMappingNames() {
+			return mappingNames;
+		}
+		public void setMappingNames(List<String> mappingNames) {
+			this.mappingNames = mappingNames;
+		}
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public String getIcon() {
+			return icon;
+		}
+		public void setIcon(String icon) {
+			this.icon = icon;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+
     // Other fields, getters, setters, and constructors
 }
